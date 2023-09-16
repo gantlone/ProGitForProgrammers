3a07e805695016585072770bc34ee9919c6f82fc
gantlone <edison0963125746@gmail.com>
2023-09-16 16:56:57 +0800

    註解123

--- ProGitForProgrammers/ProGitForProgrammers/Program.cs
+++ ProGitForProgrammers/ProGitForProgrammers/Program.cs
@@ -7,7 +7,7 @@ namespace ProGitForProgrammers
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Console.WriteLine("I just added this in Visual Studio");
+            Console.WriteLine("I just added this in Visual Studio"); //123
             Console.WriteLine("This line add to show the staging area");
             Console.WriteLine("add commit 同步");
 
