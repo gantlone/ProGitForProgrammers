412f3d46eba234ebb86eac973dc39592b4b542f4
gantlone <edison0963125746@gmail.com>
2023-09-16 16:54:23 +0800

    123

--- ProGitForProgrammers/ProGitForProgrammers/Book.cs
+++ ProGitForProgrammers/ProGitForProgrammers/Book.cs
@@ -9,7 +9,7 @@ namespace ProGitForProgrammers
     class Book
     {
         public string Title { get; set; }
-        public List<string> Authors { get; set; }
+        public List<string> Authors { get; set; } 
         public DateTime PublicationDate  { get; set; }
 
     }
